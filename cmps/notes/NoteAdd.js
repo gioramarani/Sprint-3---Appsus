a86177c79b9d800ad126a708bc7d6b47f1452c51
Giora Marani <giora.marani@gmail.com>
2023-07-05 18:04:12 +0300

    almost render a new note

--- cmps/notes/NoteAdd.js
+++ cmps/notes/NoteAdd.js
@@ -1,23 +1,37 @@
+import { NoteService } from "../../services/notes/NoteService.js"
+
 export default {
   name:'NoteAdd',
   props: [],
   template: `
             <form  @submit="onCreate" >
                 <input type="text"
-                placeholder="Take a note..." v-model="txt"/>
+                placeholder="Take a note..." v-model="txt"/> <br/>
+                <select v-model="type">Select type
+                  <option value="Txt">Text</option>
+                  <option value="Img">Image</option>
+                  <option value="Todos">To-Do's</option>
+                  <option value="Video">Video</option>
+                </select>
                 <button>Make Note</button>
                 </form>
         `,
 created() {},
   data() {
     return {
+      newNote: NoteService.getEmptyNote(),
       txt: '',
+      type: '',
     }
   },
   methods: {
     onCreate(){
-      this.$emit('create', this.txt)
-      console.log(this.txt)
+      this.$emit('create', this.newNote, this.type ,this.txt)
+      console.log(this.newNote)
+      this.txt = ''
+      this.newNote = {}
+      this.type= ''
+      console.log(this.newNote)
 
     }
   },
--- cmps/notes/NoteTxt.js
+++ cmps/notes/NoteTxt.js
@@ -6,7 +6,7 @@ export default {
             <section class="note-txt">
                 <!-- <pre>{{ note }}</pre> -->
                 <h1>{{ note.type }}</h1>
-                <h1>{{ note.id }}</h1>
+                <!-- <h1>{{ note.id }}</h1> -->
                 <h1>{{ note.info.txt }}</h1>
 
                 <button></button>
