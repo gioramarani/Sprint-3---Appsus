03f955a302b5dffa7859be310116974aefb62547
Giora Marani <giora.marani@gmail.com>
2023-07-05 10:08:43 +0300

    first check

---
+++ routes.js
@@ -0,0 +1,24 @@
+import HomePage from './views/HomePage.js'
+import AboutUs from './views/AboutUs.js'
+
+const { createRouter, createWebHashHistory } = VueRouter
+
+const routerOptions = {
+	history: createWebHashHistory(),
+	routes: [
+		{
+			path: '/',
+			component: HomePage,
+		},
+		{
+			path: '/about',
+			component: AboutUs,
+		},
+		{
+			path: '/about',
+			component: AboutUs,
+		},
+	],
+}
+
+export const router = createRouter(routerOptions)
