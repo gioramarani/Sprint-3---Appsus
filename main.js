200c588de163313970167feb6e4df8746fab2d26
Giora Marani <giora.marani@gmail.com>
2023-07-08 21:43:26 +0300

    w

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const { createApp } = Vue
 
-import { router } from '../../routes.js'
+import { router } from '/routes.js'
 
-import AppHeader from '../../cmps/AppHeader.js'
-import AppFooter from '../../cmps/AppFooter.js'
-import UserMsg from '../../cmps/UserMsg.js'
+import AppHeader from '/cmps/AppHeader.js'
+import AppFooter from '/cmps/AppFooter.js'
+import UserMsg from '/cmps/UserMsg.js'
 
 const options = {
     template: `
