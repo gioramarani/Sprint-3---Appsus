1aee9c77ca106df5995c6a6f38ce2f7878dfc6af
Gaash Shmilovich <gaash.shm@gmail.com>
2023-09-01 13:26:47 +0300

    fixing imports for typo

---
+++ img/Gamers Store_files/main.jsx
@@ -0,0 +1,15 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=bb03c588"; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_react from "/node_modules/.vite/deps/react.js?v=bb03c588"; const React = __vite__cjsImport1_react.__esModule ? __vite__cjsImport1_react.default : __vite__cjsImport1_react;
+import __vite__cjsImport2_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=bb03c588"; const ReactDOM = __vite__cjsImport2_reactDom_client.__esModule ? __vite__cjsImport2_reactDom_client.default : __vite__cjsImport2_reactDom_client;
+import App from "/src/App.jsx?t=1692966140571";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  /* @__PURE__ */ jsxDEV(App, {}, void 0, false, {
+    fileName: "/Users/gaashshmilovich/Documents/Dev/GamersStore/GamersStore/src/main.jsx",
+    lineNumber: 6,
+    columnNumber: 1
+  }, this)
+  // </React.StrictMode>,
+);
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IkFBTUU7QUFORixPQUFPQSxXQUFXO0FBQ2xCLE9BQU9DLGNBQWM7QUFDckIsT0FBT0MsU0FBUztBQUVoQkQsU0FBU0UsV0FBV0MsU0FBU0MsZUFBZSxNQUFNLENBQUMsRUFBRUM7QUFBQUE7QUFBQUEsRUFFbkQsdUJBQUMsU0FBRDtBQUFBO0FBQUE7QUFBQTtBQUFBLFNBQUk7QUFBQTtBQUVOIiwibmFtZXMiOlsiUmVhY3QiLCJSZWFjdERPTSIsIkFwcCIsImNyZWF0ZVJvb3QiLCJkb2N1bWVudCIsImdldEVsZW1lbnRCeUlkIiwicmVuZGVyIl0sInNvdXJjZXMiOlsibWFpbi5qc3giXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFJlYWN0IGZyb20gXCJyZWFjdFwiO1xuaW1wb3J0IFJlYWN0RE9NIGZyb20gXCJyZWFjdC1kb20vY2xpZW50XCI7XG5pbXBvcnQgQXBwIGZyb20gXCIuL0FwcC5qc3hcIjtcblxuUmVhY3RET00uY3JlYXRlUm9vdChkb2N1bWVudC5nZXRFbGVtZW50QnlJZChcInJvb3RcIikpLnJlbmRlcihcbiAgLy8gPFJlYWN0LlN0cmljdE1vZGU+XG4gIDxBcHAgLz5cbiAgLy8gPC9SZWFjdC5TdHJpY3RNb2RlPixcbik7XG4iXSwiZmlsZSI6Ii9Vc2Vycy9nYWFzaHNobWlsb3ZpY2gvRG9jdW1lbnRzL0Rldi9HYW1lcnNTdG9yZS9HYW1lcnNTdG9yZS9zcmMvbWFpbi5qc3gifQ==
